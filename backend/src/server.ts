d09fd956d3ec78691a6bdf2c02695e1f3e754adc
anjanaunnip <154441511+anjanaunnip@users.noreply.github.com>
2026-06-06 11:15:46 +0530

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import faqRoutes from "./routes/faq.routes";
-import aiRoutes from "./ai";   
+import aiRoutes from "./ai";
 
 dotenv.config();
 
@@ -11,12 +11,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/faqs", faqRoutes);
-app.use("/ai", aiRoutes);     
-app.get("/", (_req, res) => {
+app.use("/api/faqs", faqRoutes);
+app.use("/api/ai", aiRoutes);
+
+app.get("/api", (_req, res) => {
   res.send("FAQ API running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
